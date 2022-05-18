c1080e2d686894e8dc735d2e281b09bce3e3769a
Renan Rocha <renan.aragorn@gmail.com>
2022-05-18 14:21:34 -0300

    delete user function done

--- js/favorites.js
+++ js/favorites.js
@@ -1,12 +1,43 @@
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
+    this.load()
+
+    
   }
+
+  load() {
+    this.entries = [
+      {
+        login: 'maykbrito',
+        name: 'Mayk Brito',
+        public_repos: '76',
+        followers: '120000'
+      },
+      {
+        login: 'diego3g',
+        name: 'Diego Fernandes',
+        public_repos: '123',
+        followers: '2340'
+      }
+    ]
+  }
+
+  delete(user) {
+    const filteredEntries = this.entries.filter(entry => entry.login != user.login)
+
+    console.log(filteredEntries)
+  }
+
+  
 }
 
 export class FavoritesView extends Favorites {
   constructor (root) {
     super(root)
+
+    this.tbody = this.root.querySelector('table tbody')
+
 
     this.update()
   }
@@ -14,8 +45,32 @@ export class FavoritesView extends Favorites {
   update() {
     this.removeAllTr()
 
-    const tr = `
-    <tr>
+    this.entries.forEach( user => {
+      const row = this.createRow()
+
+      row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt = `Imagem de ${user.name}`
+      row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user span').textContent = user.login
+      row.querySelector('.repositories').textContent = user.public_repos
+      row.querySelector('.followers').textContent = user.followers
+
+      row.querySelector('.remove').onclick = () => {
+        const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+
+        if(isOk) {
+          this.delete(user)
+        }
+      }
+
+
+      this.tbody.append(row)
+    })
+  }
+
+  createRow() {
+    const tr = document.createElement('tr')
+    tr.innerHTML = `
           <td class="user">
             <img src="https://github.com/maykbrito.png" alt="imagem de maykbrito">
             <a href="https://github.com/maykbrito" target="_blank">
@@ -32,14 +87,13 @@ export class FavoritesView extends Favorites {
           <td>
             <button class="remove">&times;</button>
           </td>
-        </tr>
     `
+    return tr
   }
 
   removeAllTr() {
-    const tbody = this.root.querySelector('table tbody')
 
-    tbody.querySelectorAll('tr').forEach((tr) => {
+    this.tbody.querySelectorAll('tr').forEach((tr) => {
       tr.remove()
     })
   }
